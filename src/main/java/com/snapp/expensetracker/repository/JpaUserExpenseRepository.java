5af74488f9ae01bc82ba06e6c432de28d5b6366c
somaye <pishevar.srv@gmail.com>
2022-06-08 18:27:04 +0430

    add SwaggerConfig file and its related dependencies.

--- src/main/java/com/snapp/expensetracker/repository/JpaUserExpenseRepository.java
+++ src/main/java/com/snapp/expensetracker/repository/JpaUserExpenseRepository.java
@@ -1,19 +1,12 @@
 package com.snapp.expensetracker.repository;
 
-import com.snapp.expensetracker.common.ExpenseSumPerCategory;
-import com.snapp.expensetracker.entity.ExpenseCategory;
-import com.snapp.expensetracker.entity.User;
 import com.snapp.expensetracker.entity.UserExpense;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigInteger;
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
 
 public interface JpaUserExpenseRepository extends JpaRepository<UserExpense, Long> {
 
